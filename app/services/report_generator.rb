ef6f6e558c1a945a6b051964bc8badfc7f80e2c7
AndressaVilin <andressaestudos2024@gmail.com>
2026-08-18 13:28:30 -0300

    fix: corrige geracao de relatorios

---
+++ app/services/report_generator.rb
@@ -0,0 +1,29 @@
+class ReportGenerator
+
+  def initialize(games)
+    @games = games
+  end
+
+  def generate_games_report
+    report = {}
+
+    @games.each do |game|
+      report.merge!(game.to_h)
+    end
+
+    report
+  end
+
+  def generate_global_ranking
+    ranking = Hash.new(0)
+
+    @games.each do |game|
+      game.kills.each do |player, kills_count|
+          ranking[player] += kills_count
+      end
+    end
+
+    ranking.sort_by { |_player, kills| -kills }.to_h
+  end
+
+end
